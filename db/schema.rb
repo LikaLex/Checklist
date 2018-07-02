db30b83e83ec2b0831f1ed86dd191864bbbac0ab
Lika Lex <lika.lex555@gmail.com>
2018-07-02 09:19:14 +0300

    Add completed_at ti Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_01_103211) do
+ActiveRecord::Schema.define(version: 2018_07_02_060602) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_07_01_103211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.datetime "completed_at"
   end
 
   create_table "users", force: :cascade do |t|
